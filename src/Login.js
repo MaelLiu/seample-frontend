b0d7367e14a7d553b6ba0b3c05f82ac6dad97919
Mael Liu <owen920614@gmail.com>
2024-03-16 23:46:38 +0800

    url2

--- src/Login.js
+++ src/Login.js
@@ -4,7 +4,7 @@ import Axios from 'axios';
 import { useNavigate } from 'react-router-dom'; // For redirecting to another route
 import { useAuth } from './AuthContext'; // Import the useAuth hook
 import { TextField, Button, Container, Typography, Alert } from '@mui/material';
-const url = "https://seample-server-1606ba33c892.herokuapp.com/";
+const url = "https://seample-server-1606ba33c892.herokuapp.com";
 
 function Login() {
     const [accountName, setAccountName] = useState('');
